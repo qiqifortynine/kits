02b5cb5a29bc15695562d263812fa08ca41a508b
李洲洋 <lizhouyang19931216@163.com>
2023-10-24 14:38:25 +0800

    添加lua脚本

--- pkg/redis/kit.go
+++ pkg/redis/kit.go
@@ -2,6 +2,17 @@ package redis
 
 import "github.com/go-redis/redis/v8"
 
+const (
+	// 比较删除lua脚本
+	compareAndDeleteScript = `
+	if redis.call("GET", KEYS[1]) == ARGV[1] then
+    	return redis.call("DEL", KEYS[1])
+	else
+    	return 0
+	end
+	`
+)
+
 func NewKiter(client *redis.Client) Kiter {
 	return &Kit{client: client}
 }
